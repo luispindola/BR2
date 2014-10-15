a74e8d928e21904e344d9c232eb7ac66beb9bb4f
luispindola <luispindola78@gmail.com>
2014-10-15 12:01:58 -0700

    15oct

---
+++ application/controllers/c_tablas_esp.php
@@ -0,0 +1,19 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+class C_tablas_esp extends CI_Controller 
+{
+    public function index()
+    {
+        $this->load->model('M_creador');
+        $menu = $this->M_creador->menu();//Crear menu
+        $datos_inicio = '<h1>Tablas de especificaciones</h1>';
+        $datos_inicio = $datos_inicio.'<h2>'.$menu.'</h2>';
+        $datos_vista = array(
+        'datos_inicio'   =>  $datos_inicio,
+        'menu'           =>  $menu
+        );
+        $this->load->view('v_limpia',$datos_vista);
+    }
+}
+
+
+?>

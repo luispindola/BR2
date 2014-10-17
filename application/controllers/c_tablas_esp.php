03429444ec0de4f424800d3a6486e6a405cc15ef
luispindola <luispindola78@gmail.com>
2014-10-16 22:44:23 -0700

    16oct

--- application/controllers/c_tablas_esp.php
+++ application/controllers/c_tablas_esp.php
@@ -14,7 +14,28 @@ class C_tablas_esp extends CI_Controller
     }
     public function listado($pag=null)
     {
-        //PENDIENTE!
+        if ($this->session->userdata('id_usuario'))
+        {
+            //Si tiene sesion iniciada
+            $menu = $this->M_creador->menu();//Crear menu
+            $datos_inicio = '<h1>Listados de tablas de especificaciones</h1>';
+            
+            $datos_inicio = $datos_inicio.'<form id="form" method="post">';//Se crea una forma post
+            $datos_inicio = $datos_inicio.'<table width=70% BORDER CELLPADDING=10 CELLSPACING=0>';
+            
+                    //PENDIENTE!
+            
+            $datos_inicio = $datos_inicio.'</table>';
+            $datos_inicio = $datos_inicio.'</form>';
+                   
+            //Cargar vista vlimpia
+            $datos_vista = array(
+            'datos_inicio'   =>  $datos_inicio,
+            'menu'           =>  $menu
+            );
+            $this->load->view('v_limpia',$datos_vista);
+        }
+        else{header('Location: '.site_url('c_main'));}
     }
     public function agregar()
     {
@@ -87,7 +108,7 @@ class C_tablas_esp extends CI_Controller
             **/
             $SQL = "SELECT br_usuarios.id_usuario, br_usuarios.nivel_acceso, ";
             $SQL = $SQL."bancodereact_users.username, bancodereact_users.email, ";
-            $SQL = $SQL."br_asignaturas.asignatura, br_asignaturas.semestre, ";
+            $SQL = $SQL."br_asignaturas.asignatura, br_asignaturas.semestre, br_asignaturas.id_asignatura, ";
             $SQL = $SQL."br_tablas_esp.ciclo, Count(br_tablas_esp.id_tablas_esp) AS reactivos ";
             $SQL = $SQL."FROM (br_usuarios LEFT JOIN bancodereact_users ";
             $SQL = $SQL."ON br_usuarios.id_usuario = bancodereact_users.id) RIGHT JOIN ";
@@ -118,11 +139,66 @@ class C_tablas_esp extends CI_Controller
             $datos_inicio = $datos_inicio.'<form id="form" method="post">';//Se crea una forma post
             $datos_inicio = $datos_inicio.'<table width=70% BORDER CELLPADDING=10 CELLSPACING=0>';
             
+            $datos_inicio = $datos_inicio.'<tr><td align="center">';            
+            $datos_inicio = $datos_inicio. $this->pagination->create_links();//Se crean los links del paginador
+            $datos_inicio = $datos_inicio.'</td></tr>';   
+            $datos_inicio = $datos_inicio.'<tr><td>';
             
+            //Inicia tabla de datos
+            $datos_inicio = $datos_inicio.'<table width=100% BORDER CELLPADDING=10 CELLSPACING=0>';  
+            //Encabezados:
+            $datos_inicio = $datos_inicio.'<tr bgcolor="#517901", style="color: #FFFFFF">';//Define fondo y color de letra
+            $datos_inicio = $datos_inicio.'<th><a href="'.site_url('c_tablas_esp/usuarios_elaboradores/id_usuario/'.$pag).'"><font color="#FFFFFF">Id</font></a></th>';
+            $datos_inicio = $datos_inicio.'<th><a href="'.site_url('c_tablas_esp/usuarios_elaboradores/username/'.$pag).'"><font color="#FFFFFF">Nombre</font></a></th>';
+            $datos_inicio = $datos_inicio.'<th><a href="'.site_url('c_tablas_esp/usuarios_elaboradores/email/'.$pag).'"><font color="#FFFFFF">Correo electr&oacutenico</font></a></th>';
+            $datos_inicio = $datos_inicio.'<th><a href="'.site_url('c_tablas_esp/usuarios_elaboradores/asignatura/'.$pag).'"><font color="#FFFFFF">Asignatura</font></a></th>';
+            $datos_inicio = $datos_inicio.'<th><a href="'.site_url('c_tablas_esp/usuarios_elaboradores/semestre/'.$pag).'"><font color="#FFFFFF">Semestre</font></a></th>';
+            $datos_inicio = $datos_inicio.'<th><a href="'.site_url('c_tablas_esp/usuarios_elaboradores/ciclo/'.$pag).'"><font color="#FFFFFF">Ciclo</font></a></th>';
+            $datos_inicio = $datos_inicio.'<th><a href="'.site_url('c_tablas_esp/usuarios_elaboradores/reactivos/'.$pag).'"><font color="#FFFFFF">Reactivos</font></a></th>';
+            $datos_inicio = $datos_inicio.'<th width=10%>Acciones</th>';
+            $datos_inicio = $datos_inicio.'</tr>';
+            //Fin Encabezados
             
+            if (isset($pag))//Se agrega LIMIT a la consulta para seleccionar la pagina
+            {$SQL = $SQL.' LIMIT '.$pag.', '.$per_page;}//Si se ha seleccionado una pagina
+            else
+            {$SQL = $SQL.' LIMIT 0, '.$per_page;}//Si es la primera pagina
             
+            $query = $this->db->query($SQL);//Ejecuta la consulta
+            if ($query->num_rows() > 0)
+            {
+                foreach ($query->result() as $row)//Recorre la tabla
+                {  
+                    $datos_inicio = $datos_inicio.'<tr bgcolor="#FAFAFA",';//Color de fondo
+                    $datos_inicio = $datos_inicio.' onmouseover="this.style.backgroundColor=\'#F2F2F2\';"';//Color con mause sobre
+                    $datos_inicio = $datos_inicio.' onmouseout="this.style.backgroundColor=\'#FAFAFA\';">';//Regresar al mismo color
+                    
+                    $datos_inicio = $datos_inicio.'<td>'.$row->id_usuario.'</td>';
+                    $datos_inicio = $datos_inicio.'<td>'.$row->username.'</td>';
+                    $datos_inicio = $datos_inicio.'<td>'.$row->email.'</td>';
+                    $datos_inicio = $datos_inicio.'<td>'.$row->asignatura.'</td>';
+                    $datos_inicio = $datos_inicio.'<td>'.$row->semestre.'</td>';
+                    $datos_inicio = $datos_inicio.'<td>'.$row->ciclo.'</td>';
+                    $datos_inicio = $datos_inicio.'<td>'.$row->reactivos.'</td>';
+                    $datos_inicio = $datos_inicio.'<td>';
+                    if (isset($row->id_usuario))
+                    {
+                        $datos_inicio = $datos_inicio.'<input type="button" value="Desasignar elaborador" onClick="window.location =\''.  site_url('c_tablas_esp/desasignar_elaborador/'.$row->id_asignatura.'/'.$row->ciclo).'\';"/>';
+                    }
+                    else
+                    {
+                        $datos_inicio = $datos_inicio.'<input type="button" value="Asignar elaborador" onClick="window.location =\''.  site_url('c_tablas_esp/asignar_elaborador/'.$row->id_asignatura.'/'.$row->ciclo).'\';"/>';
+                    }
+                    $datos_inicio = $datos_inicio.'</td>';
+                    
+                    $datos_inicio = $datos_inicio.'</tr>';
+                }
+            }
             
-            
+            $datos_inicio = $datos_inicio.'</table>';
+            //Fin tabla de datos
+            $datos_inicio = $datos_inicio.'</td></tr>';
+                        
             $datos_inicio = $datos_inicio.'</table>';
             $datos_inicio = $datos_inicio.'</form>';
             $datos_inicio = $datos_inicio.'Se encontraron '.$total_rows.' registros';//comentario opcional
@@ -135,6 +211,64 @@ class C_tablas_esp extends CI_Controller
             $this->load->view('v_limpia',$datos_vista);
         }
         else{header('Location: '.site_url('c_main'));}//Si no es administrador manda al inicio
+    }
+    public function asignar_elaborador($id_asignatura = null, $ciclo = null)
+    {
+        if ($this->session->userdata('nivel_acceso') == 'Administrador')//Validar nivel de acceso de session
+        {
+            $menu = $this->M_creador->menu();//Creador de menu
+            
+            $datos_inicio = '<h1>Asignar Elaborador</h1>';
+            $datos_inicio = $datos_inicio.'<form id="form" method="post">';//Se crea una forma post
+            $datos_inicio = $datos_inicio.'<table width=50% BORDER CELLPADDING=10 CELLSPACING=0>';
+            
+            /**
+            SELECT Br_tablas_esp.id_asignatura, Br_asignaturas.semestre, 
+            Br_tablas_esp.ciclo, Br_asignaturas.componente, Br_asignaturas.asignatura, 
+            Count(Br_tablas_esp.id_tablas_esp) AS CuentaDeid_tablas_esp
+            FROM Br_tablas_esp INNER JOIN Br_asignaturas 
+            ON Br_tablas_esp.id_asignatura = Br_asignaturas.id_asignatura
+            GROUP BY Br_tablas_esp.id_asignatura, Br_asignaturas.semestre, 
+            Br_tablas_esp.ciclo, Br_asignaturas.componente, Br_asignaturas.asignatura; 
+            **/
+            $SQL = "SELECT br_tablas_esp.id_asignatura, br_asignaturas.semestre, ";
+            $SQL = $SQL."br_tablas_esp.ciclo, br_asignaturas.componente, br_asignaturas.asignatura, ";
+            $SQL = $SQL."Count(br_tablas_esp.id_tablas_esp) AS reactivos ";
+            $SQL = $SQL."FROM br_tablas_esp INNER JOIN br_asignaturas ";
+            $SQL = $SQL."ON br_tablas_esp.id_asignatura = br_asignaturas.id_asignatura ";
+            $SQL = $SQL."GROUP BY br_tablas_esp.id_asignatura, br_asignaturas.semestre, ";
+            $SQL = $SQL."br_tablas_esp.ciclo, br_asignaturas.componente, br_asignaturas.asignatura ";
+            $SQL = $SQL."HAVING ((br_tablas_esp.id_asignatura = '".$id_asignatura."') ";
+            $SQL = $SQL."AND (br_tablas_esp.ciclo = '".str_replace("%20"," ",$ciclo)."'))";
+            //DEBUG $datos_inicio = $datos_inicio.$SQL;
+            $query = $this->db->query($SQL);//Ejecuta la consulta
+            if ($query->num_rows() > 0)
+            {
+                foreach ($query->result() as $row)
+                {   
+                    $datos_inicio = $datos_inicio.'<tr><td><h2>Asignatura: </h2></td><td>'.$row->asignatura.'</td></tr>'; 
+                    $datos_inicio = $datos_inicio.'<tr><td><h2>Ciclo: </h2></td><td>'.$row->ciclo.'</td></tr>';
+                    $datos_inicio = $datos_inicio.'<tr><td><h2>Semestre: </h2></td><td>'.$row->semestre.'</td></tr>';
+                    $datos_inicio = $datos_inicio.'<tr><td><h2>Componente: </h2></td><td>'.$row->componente.'</td></tr>';
+                    $datos_inicio = $datos_inicio.'<tr><td><h2>Reactivos: </h2></td><td>'.$row->reactivos.'</td></tr>';
+                    $datos_inicio = $datos_inicio.'<tr><td><h2>Asignar usuario: </h2></td><td> desplegable </td></tr>';
+                    
+                    
+                    
+                }
+            }
+            
+            $datos_inicio = $datos_inicio.'</table>';
+            $datos_inicio = $datos_inicio.'</form>';
+            
+            //Cargar vista vlimpia
+            $datos_vista = array(
+            'datos_inicio'   =>  $datos_inicio,
+            'menu'           =>  $menu
+            );
+            $this->load->view('v_limpia',$datos_vista);
+        }
+        else{header('Location: '.site_url('c_main'));}
     }
 }
 ?>
